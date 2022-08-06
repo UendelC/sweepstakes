f2d0cf44e82342722de7ac5cf935d09a0b314e91
uendel.couto <uendel.couto@gmail.com>
2022-08-05 21:59:04 -0300

    creating sweepstake model and its relationship with user

---
+++ app/Models/Sweepstake.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Sweepstake extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'number_of_winners',
+        'end_date',
+        'description',
+    ];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(static function (Model $model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
