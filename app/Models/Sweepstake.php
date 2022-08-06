952721d1a8889e0dbe844ea80ffc0e1657e3e2d4
uendel.couto <uendel.couto@gmail.com>
2022-08-05 22:47:50 -0300

    create relationship between sweepstake and participants

--- app/Models/Sweepstake.php
+++ app/Models/Sweepstake.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Sweepstake extends Model
@@ -36,5 +37,10 @@ class Sweepstake extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function participants(): HasMany
+    {
+        return $this->hasMany(Participant::class);
     }
 }
